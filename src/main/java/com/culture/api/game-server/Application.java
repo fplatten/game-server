06385631275e9cec2bdfb02930a8ea253dd0acee
fplatten <fplatten@gmail.com>
2017-12-16 10:03:44 -0500

    adding artifacts and pom

---
+++ src/main/java/com/culture/api/game-server/Application.java
@@ -0,0 +1,16 @@
+package com.culture.api.game-server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+
+		// to run from maven: mvn spring-boot:run
+		// to create war file for remote deployment: mvn -Pdeploy package
+		SpringApplication.run(Application.class, args);
+	}
+
+}
